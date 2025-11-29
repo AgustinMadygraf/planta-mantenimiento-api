609515596a0c672e4ee548cad40651798060d083
AgustinMadygraf <agustin.mtto.madygraf>
2025-11-29 13:34:40 -0300

    Add SSL context to Flask app run configuration

--- run_flask.py
+++ run_flask.py
@@ -5,7 +5,12 @@ from src.infrastructure.flask.app import create_app
 
 def main() -> None:
     app = create_app()
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=8000,
+        debug=True,
+        ssl_context=("certs/cert.pem", "certs/key.pem"),
+    )
 
 
 if __name__ == "__main__":
